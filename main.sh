9203dc59648ad58b274dde52b29cb98b9cff4bdd
Yuto <louise.insomnia@gmail.com>
2022-03-22 15:15:37 +0100

    test

--- main.sh
+++ main.sh
@@ -1,6 +1,18 @@
 #!/bin/bash   
 
+# create alacritty config file and add theme to it 
 mkdir ~/.config/alacritty
 cp alacritty.yml ~/.config/alacritty  
+
+# create nvim config file   
+mkdir ~/.config/nvim
+cp init.vim ~/.config/nvim 
+
+
+# install vim plug to add vim plugins 
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Now you should be able to :PlugInstall in neovim
 
 
